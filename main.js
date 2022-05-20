4f6316076236738e7303fdc6cea7504544928b80
tiemtv <tiemtv@awing.vn>
2022-05-20 08:59:44 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const textFields3 = [
     { type: 'checkbox', ask: 'Cau hoi 3', answer: 'cau 3 \n lcau333' },
     { type: 'radio', ask: 'Cau hoi 4', answer: 'cau 4 \n cau 4.1 \n 4.2' },
     { type: 'radio', ask: 'Cau hoi 5', answer: 'cau ans \n cau fsg4ws \n gsdfvsd' },
-    { type: 'checkbox', ask: 'Cau hoi 6', answer: 'cau ans \n cau lcfsg4ws \n dfvsd' }
+    { type: 'checkbox', ask: 'Cau hoi 6', answer: 'trả lời 1 \n lctrả lời 2 \n trả lời 3 \n lctrả lời 4' }
 ];
 
 // var textFields3 = { ten_truong_3 };
@@ -20,16 +20,15 @@ for (let i = 0; i < textFields3.length; i++) {
         if (myArrayRadio[j].includes('lc')) {
             var myArrayCheckbox3 = myArrayRadio[j].split("lc");
             indexAnswerOrther = j;
-            textAddHtml += '<div class="ask d1-flex mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + ' khac' + i + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + 'class="mt-3px answer"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:red">' + myArrayCheckbox3[1] + '</p></label></div>' +
-                '<div id="khac' + i + j + '"' + 'style="margin-left:30px;">' +
-                '<textarea placeholder="Vui lòng điền thêm thông tin tại đây" class="text-area textA" id="khac' + i + '" required></textarea> </div>';
-
+            textAddHtml += '<div class="ask d1-flex mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + ' khac' + i + j  + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + 'class="mt-3px answer"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:red">' + myArrayCheckbox3[1] + '</p></label></div>' +
+                '<div id="div-khac' + i + j + '"' + 'style="margin-left:30px;" >' +
+                '<textarea placeholder="Vui lòng điền thêm thông tin tại đây" class="text-area textA" id="khac' + i + j  + '" required></textarea> </div>';
         } else {
 
             textAddHtml += '<div class="ask d1-flex   mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + ' class="mt-3px answer"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:green">' + myArrayRadio[j] + '</p></label></div>';
         }
-
     }
+    textAddHtml += '</form> </div> </li>';
     arrayAnswer.push({
         id: i,
         indexAnswerOrther: indexAnswerOrther,
@@ -37,79 +36,47 @@ for (let i = 0; i < textFields3.length; i++) {
         valid: false,
         answer: []
     })
-    textAddHtml += '</form> </div> </li>';
 }
-// console.log(textAddHtml);
 $("#q-scroll .list-question").html(textAddHtml);
 
-// var newtextFields3 = textFields3.map(changeField);
-
-// function changeField(textFields3) {
-//     return {
-//         type: textFields3.type,
-//         cauhoi: textFields3.ask,
-//         traloi: textFields3.answer.split("\n"),
-//     };
-// };
-
-
-// console.log(arrayAnswer);
-
 function goToHomePage() {
-    // var valuecc = [];
-
-    // $('.q-form').each(function(index) {
-    //     // console.log($(this));
-    //     console.log(index);
-    //     // valuecc.push($(this).val());
-    // });
-    // console.log(valuecc);
-
-    arrayAnswer = getAnswer(arrayAnswer, ".class-awing");
-    console.log(arrayAnswer);
-
+    var newArrayAnswer = getAnswer(arrayAnswer, ".class-awing");
+    console.log(newArrayAnswer,"newArrayAnswer");
     // Validation: Tất cả valid trong arrayAnswer = true. => post dữ liệu về
     // Ngược lại hiện thông báo
 }
 
-function getAnswer(arrayAnswer, elementClass) {
+function getAnswer(newArrayAnswer, elementClass) {
+    newArrayAnswer = newArrayAnswer.map((x) => {x.valid = false; x.answer = []; return x;})
     $(elementClass).each(function(index) {
         var inputName = '.traloi' + index + ':checked';
         var newAnswer = $(inputName).length;
-        // if (newAnswer > 0) {
-        //     arrayAnswer[index].value = newAnswer;
-        //     arrayAnswer[index].valid = true;
-        //     var question = []
-        //     $("input[name=question" + index + "]:checked").each(function() {
-        //         question.push($(this).val());
-        //     });
-        //     arrayAnswer[index].answer = question;
-        // }
         if (newAnswer > 0) {
-            arrayAnswer[index].value = newAnswer;
-
+            newArrayAnswer[index].value = newAnswer;
             let valid = false;
             var question = []
-
-            $("input[name=question" + index + "]:checked").each(function() {
-                console.log($(this))
-                question.push($(this).val());
+            $("input[name=question" + index + "]").each(function(newIndex) {
+                if(this.checked){
+                    let inputNameDiff = '.khac' + index + newIndex + ':checked';
+                    var newAnswerKhac = $(inputNameDiff).length;
+                    var dien = $('#khac' + index + newIndex).val();
+                    if(newAnswerKhac > 0){
+                        if(dien.trim()){
+                            valid = true;
+                            var questionF = $(inputNameDiff).val() + "-" + dien;
+                            question.push(questionF);;
+                        }else  valid = false;
+                    }else {
+                        valid = true;
+                        question.push(this.value);
+                    }
+                }
             });
-            arrayAnswer[index].answer = question;
-
-            let inputNameDiff = '.khac' + index + ':checked';
-            var newAnswerKhac = $(inputNameDiff).length;
-            var dien = $('#khac' + index).val();
-
-            if (arrayAnswer[index].indexAnswerOrther >= 0 && newAnswerKhac > 0 && dien.trim()) {
-                valid = true;
-                var questionF = $(inputNameDiff).val() + "-" + dien;
-                question[arrayAnswer[index].indexAnswerOrther] = questionF;
-            }
-            arrayAnswer[index].valid = valid;
+            newArrayAnswer[index].answer = question;
+            newArrayAnswer[index].valid = valid;
         }
     });
-    return arrayAnswer;
+    return newArrayAnswer;
 }
 
 function getAlphabet(index) {
